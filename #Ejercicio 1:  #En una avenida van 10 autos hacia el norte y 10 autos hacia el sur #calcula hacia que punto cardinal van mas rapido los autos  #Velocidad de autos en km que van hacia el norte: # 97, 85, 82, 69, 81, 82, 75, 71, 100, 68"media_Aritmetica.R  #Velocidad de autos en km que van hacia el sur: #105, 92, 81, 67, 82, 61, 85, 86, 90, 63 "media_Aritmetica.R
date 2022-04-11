d5d2b3d7c77e624373d510b19a223a024aa148d5
montse386 <103452448+montse386@users.noreply.github.com>
2022-04-11 14:17:04 -0500

    Create #Ejercicio 1:  #En una avenida van 10 autos hacia el norte y 10 autos hacia el sur #calcula hacia que punto cardinal van mas rapido los autos  #Velocidad de autos en km que van hacia el norte: # 97, 85, 82, 69, 81, 82, 75, 71, 100, 68"media_Aritmetica.R  #Velocidad de autos en km que van hacia el sur: #105, 92, 81, 67, 82, 61, 85, 86, 90, 63 "media_Aritmetica.R

---
+++ #Ejercicio 1:  #En una avenida van 10 autos hacia el norte y 10 autos hacia el sur #calcula hacia que punto cardinal van mas rapido los autos  #Velocidad de autos en km que van hacia el norte: # 97, 85, 82, 69, 81, 82, 75, 71, 100, 68"media_Aritmetica.R  #Velocidad de autos en km que van hacia el sur: #105, 92, 81, 67, 82, 61, 85, 86, 90, 63 "media_Aritmetica.R
@@ -0,0 +1,10 @@
+#Ejercicio 1:
+
+#En una avenida van 10 autos hacia el norte y 10 autos hacia el sur
+#calcula hacia que punto cardinal van mas rapido los autos
+
+#Velocidad de autos en km que van hacia el norte:
+# 97, 85, 82, 69, 81, 82, 75, 71, 100, 68
+#"media_Aritmetica.R
+#Velocidad de autos en km que van hacia el sur:
+#105, 92, 81, 67, 82, 61, 85, 86, 90, 63 
